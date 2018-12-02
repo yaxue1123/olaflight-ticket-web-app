e945400a5625c5c4101d00b133d7f787bade2c0a
yaxueguo <yaxue.guo@outlook.com>
2018-12-02 14:07:37 -0500

    add search function

---
+++ data/flight_data.js
@@ -0,0 +1,102 @@
+let all_flights = [
+    {
+        "departs_at": 8:00,
+        "arrives_at": 9:35,
+        "number": "Flight 2126",
+        "plane_id": 1,
+        "departure_id": 1,
+        "arrival_id": 4,
+        "airline_id": 2,
+        "info": "164"
+    }, 
+    {
+        "departs_at": 12:15,
+        "arrives_at": 14::00,
+        "number": "Flight 774",
+        "plane_id": 1,
+        "departure_id": 1,
+        "arrival_id": 4,
+        "airline_id": 2,
+        "info": "148"
+    }, 
+    {
+        "departs_at": 8:25,
+        "arrives_at": 10:11,
+        "number": "Flight 5350",
+        "plane_id": 1,
+        "departure_id": 1,
+        "arrival_id": 2,
+        "airline_id": 1,
+        "info": "180"
+    }, 
+    {
+        "departs_at": 7:54,
+        "arrives_at": 9:45,
+        "number": "Flight 4050",
+        "plane_id": 1,
+        "departure_id": 1,
+        "arrival_id": 3,
+        "airline_id": 1,
+        "info": "171"
+    },
+    {
+        "departs_at": 7:53,
+        "arrives_at": 9:31,
+        "number": "Flight 1186",
+        "plane_id": 1,
+        "departure_id": 1,
+        "arrival_id": 2,
+        "airline_id": 3,
+        "info": "166"
+    },
+    {
+        "departs_at": 9:00,
+        "arrives_at": 10:35,
+        "number": "Flight 2127",
+        "plane_id": 1,
+        "departure_id": 4,
+        "arrival_id": 1,
+        "airline_id": 2,
+        "info": "164"
+    }, 
+    {
+        "departs_at": 13:15,
+        "arrives_at": 15::00,
+        "number": "Flight 775",
+        "plane_id": 1,
+        "departure_id": 4,
+        "arrival_id": 1,
+        "airline_id": 2,
+        "info": "148"
+    }, 
+    {
+        "departs_at": 9:25,
+        "arrives_at": 11:11,
+        "number": "Flight 5351",
+        "plane_id": 1,
+        "departure_id": 2,
+        "arrival_id": 1,
+        "airline_id": 1,
+        "info": "180"
+    }, 
+    {
+        "departs_at": 6:54,
+        "arrives_at": 8:45,
+        "number": "Flight 4051",
+        "plane_id": 1,
+        "departure_id": 3,
+        "arrival_id": 1,
+        "airline_id": 1,
+        "info": "171"
+    },
+    {
+        "departs_at": 9:53,
+        "arrives_at": 11:31,
+        "number": "Flight 1187",
+        "plane_id": 1,
+        "departure_id": 2,
+        "arrival_id": 1,
+        "airline_id": 3,
+        "info": "166"
+    }
+]
