5347e88bd8145bb4c04014b1fef0382606ca525b
yaxue1123 <yaxue@live.unc.edu>
2018-12-02 14:07:37 -0500

    add search function

---
+++ data/flight.js
@@ -0,0 +1,43 @@
+let all_flights = [
+    {
+        "departs_at": 8:00,
+        "arrives_at": 9:35,
+        "number": ,
+        "plane_id": ,
+        "departure_id": ,
+        "arrival_id": ,
+        "airline_id": ,
+        "info": "136"
+    }, 
+    {
+        "departs_at": ,
+        "arrives_at": ,
+        "number": ,
+        "plane_id": ,
+        "departure_id": ,
+        "arrival_id": ,
+        "airline_id": ,
+        "info": ""
+    }, 
+    {
+        "departs_at": ,
+        "arrives_at": ,
+        "number": ,
+        "plane_id": ,
+        "departure_id": ,
+        "arrival_id": ,
+        "airline_id": ,
+        "info": ""
+    }, 
+    {
+        "departs_at": ,
+        "arrives_at": ,
+        "number": ,
+        "plane_id": ,
+        "departure_id": ,
+        "arrival_id": ,
+        "airline_id": ,
+        "info": ""
+    }
+
+]
