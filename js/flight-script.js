f8218fb1ee7786fe3ebd20598511f4552b1cc6f9
Yu Yuan <yyu03@live.unc.edu>
2018-12-04 15:22:32 -0500

    add seat map on the checkout page & construct the style

--- js/flight-script.js
+++ js/flight-script.js
@@ -14,24 +14,36 @@ $(document).ready(() => {
   //listen to the click button
   $('body').on('click', '.search-btn', function() {
     let record = get_search_input();
+    console.log(record)
     //remove the current div
+    $(".title_div").children("#sub-banner").remove();
+    $(".search_div").remove();
+    $(".search_div").toggleClass("show_ticket");
     $(".content_div").children().remove();
     $("body").attr("id", "sub");
-
     $(".title_div").children("#title-img").hide();
     $(".title_div").append("<img id='sub-banner' src='./image/sub-banner.png' alt='sub-logo'>")
+
     //call to load a new mode
     set_result_page(record);
+    datepicker_voke();
   });
 
   // ##################### P2 -> P3: select + fill info ##################
   $('body').on('click', '.checkout', function() {
     let record = get_search_input();
     let flight_id = $(this).attr("id");
-    let body = $('body');
+    let div_to_change = $('.content_div');
 
-    // clear body, new page.
-    body.empty();
+    // clear content_div & the search_div
+    div_to_change.empty().attr("id", "select_seat");
+    $('.search_div').empty()
+      .toggleClass("show_ticket");
+
+
+
+
+
 
     // show selected flight info.
     $.ajax({
@@ -41,7 +53,10 @@ $(document).ready(() => {
         withCredentials: true
       },
       success: (response) => {
-        show_one_flight(response, "one");
+        let new_div = show_one_flight(response, "one");
+        //add flight info
+        $('.search_div').append(new_div);
+
       }
     });
 
@@ -59,32 +74,68 @@ $(document).ready(() => {
         }
       },
       success: (response) => {
-        body.append('instance id: <a id="instance-id">' + response.id + '</a>');
+        customer_container.append('<br><button class="order" id = ' + response.id + '>Order</button>');
+        // customer_container.append('instance id: <a id="instance-id">' + response.id + '</a>');
         console.log("success create instance!!!");
       }
     });
 
-    body.append('Flight id: <div id="flight-id">' + flight_id + '</div>');
+    //initiate the seat map
+    let [map, types] = show_seat();
+    let sc = new SeatchartJS(map, types);
+    let currency = "";
+    sc.setAssetsSrc("./image/assets");
+    sc.setCurrency(currency);
+    sc.setSoundEnabled(false);
+
+    div_to_change.append('<div id="map-container"></div>')
+      .append('<div class="containers"></div>');
+    $('.containers').append('<div id="legend-container"></div>');
+
+    // create functions for seat
+    sc.createMap("map-container");
+    sc.createLegend("legend-container");
+    div_to_change.append('<div class = "customer_container"></div>')
+      .append('<div class = "flight_container"></div>');
+
+    // layout the passenger information
+    let customer_container = div_to_change.children(".customer_container");
+
+    customer_container.append('<p class="flight-id" id = ' + flight_id + '>Customer Info</p>');
     // read user input of first name, last name, age, gender, seat.
-    body.append('First name:<input id="first-name"></input><br>');
-    body.append('Last name:<input id="last-name"></input><br>');
-    body.append('Age:<input id="age"></input><br>');
-    body.append('Gender:<input id="gender"></input><br>');
+    customer_container.append('<span>first name</span><input id="first-name"></input><br>');
+    customer_container.append('<span>last name</span><input id="last-name"></input><br>');
+    customer_container.append('<span>age</span><input id="age"></input><br>');
+    customer_container.append('<span>gender</span><select id = "gender-options"><option value="male">Male</option><option value="female">Female</option></select><br>');
+    customer_container.append('<span>seat</span><input id="seat-row" readonly="readonly"></input><input id="seat-number" readonly="readonly" ></input><br>');
 
-    // TO DO: SVG SEAT MAP CONNECTED WITH DB. !!!!!!!!!!!!!!!!!!!!!!!
-    // TO DO: change to drop down menu.
-    body.append('seat row:<input id="seat-row"></input><br>');
-    body.append('seat number:<input id="seat-number"></input><br>');
-
-    // row and number via user input.
-
-    body.append('<br><button id="order">Order</button>');
+    //listen to the seat selecting
+    $('body').on('click', '.seatChart-seat', function(e) {
+      //clear the previous clicking
+      $('.seatChart-container .seatChart-seat').each(function() {
+        if ($(this).hasClass("clicked") && !$(this).hasClass("legend-style")) {
+          if ($(this)[0].innerHTML != e.currentTarget.innerHTML) {
+            console.log("NOoooo");
+            $(this).removeClass("clicked")
+              .css("background-color", "#7189c0");
+          }
+        }
+      })
+      //auto fill the seat info
+      customer_container.children("#seat-row").val('');
+      customer_container.children("#seat-number").val('');
+      let seat_row = e.currentTarget.innerHTML[0];
+      let seat_number = e.currentTarget.innerHTML[1];
+      console.log(seat_row, seat_number);
+      customer_container.children("#seat-row").val(seat_row);
+      customer_container.children("#seat-number").val(seat_number);
+    });
 
   });
 
   // ###################### P3: create ticket ############################
-  $('body').on('click', '#order', function() {
-    let flight_id = $('#flight-id').text();
+  $('body').on('click', '.order', function() {
+    let flight_id = $('.flight-id').attr("id");
 
     // choose seat and create a seat object.
     // require: plane id, row (1, 2, ...), and number ('A', 'B', ...)
@@ -127,15 +178,43 @@ $(document).ready(() => {
 
 });
 
+var show_seat = function() {
+  // ###################### Try: show seat ##############################
+  let map = {
+    rows: 9,
+    cols: 9,
+    // e.g. Reserved Seat [Row: 1, Col: 2] = 7 * 1 + 2 = 9
+    reserved: [1, 2, 3, 5, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21],
+    disabled: [0, 8],
+    disabledRows: [4],
+    disabledCols: [4]
+  };
+
+  let types = [{
+    type: "regular",
+    color: "#d46d36",
+    price: ""
+  }, ];
+
+  return [map, types];
+}
+
 var show_one_flight = function(one_flight, input) {
-  let c_div = $('<div class="flight" id="' + one_flight.number + '"></div>');
+  let c_div;
+  let flight_info = {};
+  if (input === "all") {
+    c_div = $('<div class="flight" id="' + one_flight.number + '"></div>');
+  } else {
+    c_div = $('<div class="a-flight" id="' + one_flight.number + '"></div>');
+  }
+
   // in millisecond.
   let duration = Math.abs(moment(one_flight.departs_at) - moment(one_flight.arrives_at));
   let hour = parseInt(duration / 3600000);
   let minute = Math.round((duration / 3600000 - hour) * 60);
 
-  //add space for image
-  c_div.append('<div class = "aireline_img"></div>');
+  // ***** copy an object of one flight. *****
+  flight_info = one_flight;
 
   //add flight info
   c_div.append('<div class = "flight_info"></div>');
@@ -147,6 +226,7 @@ var show_one_flight = function(one_flight, input) {
   // airport info.
   search_airport(one_flight);
 
+
   // price.
   c_div.append('<div class = "price">$' + one_flight.info + '</div>');
   //checkout
@@ -154,6 +234,7 @@ var show_one_flight = function(one_flight, input) {
     c_div.append('<button class = "checkout" id="' + one_flight.id + '">Check</button>');
   }
 
+  // return [c_div, flight_info];
   return c_div;
 
   // input: flight object.
@@ -180,6 +261,10 @@ var show_one_flight = function(one_flight, input) {
           .after('<div class = "airport_info">' +
             result.departure.code + ' -> ' +
             result.arrival.code + '<div>');
+
+        // ***** copy an object of one flight. *****
+        flight_info.departure_airport = result.departure.code;
+        flight_info.arrival_airport = result.arrival.code;
       }
     });
   }
@@ -200,16 +285,110 @@ var show_one_flight = function(one_flight, input) {
     function search_airline(response, id) {
       for (let prop in response) {
         if (response[prop].id === id) {
-          c_div.children(".flight_info").children(".flight-wrap").append('<span class = "airline_name">' + response[prop].name + '</span>');
-          c_div.children(".flight_info").children(".flight-wrap").append('<span class = "flight_number">' + one_flight.number + '</span></br>')
-            .append('<span class = "time_scope">' + (one_flight.departs_at).substring(12, 16) + ' - ' + (one_flight.arrives_at).substring(12, 16) + '</span>')
+          //add space for image
+          c_div.append('<img class = "airline_img" src="' + response[prop].logo_url + '">');
+
+          c_div.children(".flight_info").children(".flight-wrap")
+            .append('<span class = "airline_name">' + response[prop].name + '</span>');
+          c_div.children(".flight_info").children(".flight-wrap")
+            .append('<span class = "flight_number">' + one_flight.number + '</span></br>')
+            .append('<span class = "time_scope">' +
+              moment(one_flight.departs_at).format('LT') + ' - ' +
+              moment(one_flight.arrives_at).format('LT') + '</span>')
             .append('<span class = "duration">' + hour + 'h' + minute + 'min</span>');
+
+          // ***** copy an object of one flight. *****
+          flight_info.airline_logo = response[prop].logo_url;
+          flight_info.airline_name = response[prop].name;
         }
       }
     }
   }
-
 }
+
+// var show_one_flight = function(one_flight, input) {
+//   let c_div = $('<div class="flight" id="' + one_flight.number + '"></div>');
+//   // in millisecond.
+//   let duration = Math.abs(moment(one_flight.departs_at) - moment(one_flight.arrives_at));
+//   let hour = parseInt(duration / 3600000);
+//   let minute = Math.round((duration / 3600000 - hour) * 60);
+//
+//   //add space for image
+//   c_div.append('<div class = "aireline_img"></div>');
+//
+//   //add flight info
+//   c_div.append('<div class = "flight_info"></div>');
+//   c_div.children(".flight_info").append("<div class='flight-wrap'></div>");
+//
+//   //pair the airline
+//   pair_airline(one_flight.airline_id);
+//
+//   // airport info.
+//   search_airport(one_flight);
+//
+//   // price.
+//   c_div.append('<div class = "price">$' + one_flight.info + '</div>');
+//   //checkout
+//   if (input === "all") {
+//     c_div.append('<button class = "checkout" id="' + one_flight.id + '">Check</button>');
+//   }
+//
+//   return c_div;
+//
+//   // input: flight object.
+//   function search_airport(flight) {
+//     let result = {};
+//     // airorts.
+//     $.ajax({
+//       url: root_url + 'airports',
+//       type: 'GET',
+//       xhrFields: {
+//         withCredentials: true
+//       },
+//       success: (response) => {
+//         for (let prop in response) {
+//           if (response[prop].id == flight.departure_id) {
+//             result.departure = response[prop];
+//           }
+//
+//           if (response[prop].id == flight.arrival_id) {
+//             result.arrival = response[prop];
+//           }
+//         }
+//         c_div.children(".flight_info").children(".flight-wrap")
+//           .after('<div class = "airport_info">' +
+//             result.departure.code + ' -> ' +
+//             result.arrival.code + '<div>');
+//       }
+//     });
+//   }
+//
+//   function pair_airline(airlineId) {
+//     // airline.
+//     $.ajax({
+//       url: root_url + 'airlines',
+//       type: 'GET',
+//       xhrFields: {
+//         withCredentials: true
+//       },
+//       success: (response) => {
+//         search_airline(response, airlineId);
+//       }
+//     });
+//
+//     function search_airline(response, id) {
+//       for (let prop in response) {
+//         if (response[prop].id === id) {
+//           c_div.children(".flight_info").children(".flight-wrap").append('<span class = "airline_name">' + response[prop].name + '</span>');
+//           c_div.children(".flight_info").children(".flight-wrap").append('<span class = "flight_number">' + one_flight.number + '</span></br>')
+//             .append('<span class = "time_scope">' + (one_flight.departs_at).substring(12, 16) + ' - ' + (one_flight.arrives_at).substring(12, 16) + '</span>')
+//             .append('<span class = "duration">' + hour + 'h' + minute + 'min</span>');
+//         }
+//       }
+//     }
+//   }
+//
+// }
 
 var show_search_result = function(info) {
 
@@ -245,11 +424,12 @@ var set_result_page = function(input) {
   $(".search_div").append('<button class="search-btn" id = "sub-search">Search</button>');
   //set default value
   datepicker_voke();
+  console.log(input['date']);
   $("#datepicker").datepicker('setDate', input['date'])
     .css("width", "50%");
   airport_compelete();
-  let d_input = input['depart_text'].split(",")[0];
-  let a_input = input['arrive_text'].split(",")[0];
+  let d_input = input['depart_text'];
+  let a_input = input['arrive_text'];
   $("#depart").val(d_input).css("font-size", "15px").css("color", "#3f3f3f");
   $("#arrive").val(a_input).css("font-size", "15px").css("color", "#3f3f3f");
 
